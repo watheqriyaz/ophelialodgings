8905b0862d4b06c762fafeffca7c0ad850180d4b
watheqriyaz <144660580+watheqriyaz@users.noreply.github.com>
2023-09-11 14:29:14 +0530

    Delete Scripts directory

--- Scripts/main.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function() {
-    const header = document.querySelector('header');
-    header.classList.toggle('scrolled', window.scrollY > 0);
-  });
-
-$(document).ready(function() {
-    $('.fade-in').addClass('fade-in');
-  });
